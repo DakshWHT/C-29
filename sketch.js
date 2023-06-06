362cce5cc03373d37dd7542630d996e76b79096d
DakshWHT <93590885+DakshWHT@users.noreply.github.com>
2023-06-06 18:02:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+let ground;
+let rope;
+
+function setup() 
+{
+  createCanvas(500,700);
+  engine = Engine.create();
+  world = engine.world;
+ 
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+
+  ground = new Ground(50, 500, 500, 100);
+
+}
+
+function draw() 
+{
+  background(51);
+
+  //  Engine.update(engine); 
+
+  ground.show();
+
+  rope.show();
+}
+
+
+
+
